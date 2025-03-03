b9dcf73159fc5af0e269a24f654872241c6b27e4
Ashutosh Kumar <ashutosh@example.com>
2025-03-03 14:25:19 +0530

    updated

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const setupMiddleware = require('./middleware/middleware');
+const setupMiddleware = require('./middllewares/middleware');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
